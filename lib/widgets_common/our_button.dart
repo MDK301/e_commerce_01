c354e1e91b5b753e55c02a6459aa93acfcc1edad
Trysor-llachar <trysorllachar@gmail.com>
2024-09-24 16:04:19 +0700

    fix it, it's about the way of using title in our_button.dart child: title.text.color(textColor).fontFamily(bold).make() => Text( title,style: TextStyle( color: textColor, fontFamily: bold,),                                                                  ),

--- lib/widgets_common/our_button.dart
+++ lib/widgets_common/our_button.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce/consts/consts.dart';
-export 'package:velocity_x/velocity_x.dart';
 
-Widget ourButton({color ,textColor ,title ,onPress ,}) {
+Widget ourButton({
+  color,
+  textColor,
+  title,
+  onPress,
+}) {
   return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: color,
-        padding: const EdgeInsets.all(12),
-      ),
-      onPressed: () {
-        onPress;
-      },
-  child: VxText(title).color(textColor).fontFamily(bold).make(),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: color,
+      padding: const EdgeInsets.all(12),
+    ),
+    onPressed: () {
+      onPress;
+    },
+    child: Text(title,style: TextStyle(color: textColor,fontFamily: bold,),),
+  );
 }
